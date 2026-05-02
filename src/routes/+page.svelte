3fb85a04d858a594dc2235d26c9f91584479856a
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-05-02 17:14:52 +0900

    feat: display simple FPS.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	// Based on: https://stackoverflow.com/a/5111475
+	// The higher this value, the less the fps will reflect temporary variations
+	// A value of 1 will only keep the last value
+	let filterStrength = 10;
+
+	let frameTime = $state(0);
+
+	let lastLoop = performance.now();
+	let thisLoop = lastLoop;
+
+	function updateFps() {
+		thisLoop = performance.now();
+		let thisFrameTime = thisLoop - lastLoop;
+		frameTime += (thisFrameTime - frameTime) / filterStrength;
+		lastLoop = thisLoop;
+	}
+
+	let displayFrameTime = $state('0');
+	let displayFps = $state('0');
+
+    onMount(() => {
+		setInterval(() => {
+			updateFps();
+		});
+
+		setInterval(() => {
+			displayFrameTime = frameTime.toFixed(3);
+			displayFps = (1000 / frameTime).toFixed(1);
+		}, 500);
+	});
+</script>
+
+{displayFps} FPS<br />
+{displayFrameTime} ms
