c653da8d843f4da94396c7a0eec0ded376676fb8
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-05-05 13:33:51 +0900

    perf: precompute colors in palettes.

--- src/lib/generateNoise.ts
+++ src/lib/generateNoise.ts
@@ -1,3 +1,21 @@
+// Precompute grayscale palette
+const palette = new Uint32Array(256);
+for (let i = 0; i < 256; i++) {
+	const r = i,
+		g = i,
+		b = i,
+		a = 255;
+	palette[i] = (a << 24) | (b << 16) | (g << 8) | r;
+}
+
+const paletteCyan = new Uint32Array(256);
+for (let i = 0; i < 256; i++) {
+	const g = i,
+		b = i,
+		a = 255;
+	paletteCyan[i] = (a << 24) | (b << 16) | (g << 8);
+}
+
 const chunkSize = 65536; // max allowed
 
 export function generateNoise(data: Uint8Array) {
@@ -12,18 +30,14 @@ export function renderNoise(noise: Uint8Array, imageData: ImageData, skipRed = f
 	// reinterpret the buffer as 32‑bit words
 	const data32 = new Uint32Array(imageData.data.buffer);
 
-	for (let i = 0; i < noise.length; i++) {
-		const value = noise[i];
-
-		// pack RGBA into one 32‑bit integer
-		// little‑endian order: lowest byte is R, then G, B, A
-		const r = skipRed ? 0 : value;
-		const g = value;
-		const b = value;
-		const a = 255;
-
-		data32[i] = (a << 24) | (b << 16) | (g << 8) | r;
+	if (skipRed) {
+		for (let i = 0; i < noise.length; i++) {
+			data32[i] = paletteCyan[noise[i]];
+		}
+	} else {
+		for (let i = 0; i < noise.length; i++) {
+			data32[i] = palette[noise[i]];
+		}
 	}
-
 	return imageData;
 }
