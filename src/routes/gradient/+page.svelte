5b4df2c5e542838b29833cc76a4980c545314c4d
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-05-07 01:46:25 +0900

    feat: adjust gradient orientation.

--- src/routes/gradient/+page.svelte
+++ src/routes/gradient/+page.svelte
@@ -15,7 +15,7 @@
 
 		for (let j = 0; j < height; j++) {
 			for (let i = 0; i < height; i++) {
-				const value = size - Math.min(Math.abs(i - j - size));
+				const value = size - Math.min(Math.abs(i - j + size));
 				grid[i + j * width] = value;
 			}
 		}
