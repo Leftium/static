fcd2a4320d0082db280ef50044ea9337600a1606
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-05-11 20:41:12 +0900

    feat: add hidden image to static.

--- src/routes/static/+page.svelte
+++ src/routes/static/+page.svelte
@@ -4,24 +4,50 @@
 	import GraphicalEffect from '$lib/GraphicalEffect.svelte';
 
 	let imageData: ImageData;
-	let noise = new Uint8Array(0);
+	let noisePrev = new Uint8Array(0);
+	let noiseNext = new Uint8Array(0);
 </script>
 
 <main>
 	<GraphicalEffect
 		oninit={(fx: FxState) => {
 			//console.log('init', $state.snapshot(fx));
-			fx.scalingFactor = 1 / 2;
+			fx.scalingFactor = 1 / 4;
 			// Pixel ratio based on NTSC 440x486 resolution stretched to 4:3 aspect ratio.
 			fx.pixelAspectRatio = ((4 / 440) * 486) / 3;
 		}}
 		onresize={(_fx, width, height) => {
 			console.log('resizeHandler', { width, height });
 			imageData = createOpaqueImageData(width, height);
-			noise = new Uint8Array(width * height);
+			noisePrev = new Uint8Array(width * height);
+			noiseNext = new Uint8Array(width * height);
+
+			generateNoiseUint8(noisePrev);
+			generateNoiseUint8(noiseNext);
 		}}
-		onupdate={() => generateNoiseUint8(noise)}
-		onrender={() => renderNoisePalette(noise, imageData)}
+		onupdate={(fx) => {
+			[noisePrev, noiseNext] = [noiseNext, noisePrev];
+
+			generateNoiseUint8(noiseNext);
+
+			for (let j = 0; j < imageData.height; j++) {
+				for (let i = 0; i < imageData.width; i++) {
+					const gridX = Math.floor(i / (100 / fx.pixelAspectRatio)) % 2;
+					const gridY = Math.floor(j / 100) % 2;
+
+					if (gridX == 0 && gridY == 0 && Math.random() < 1) {
+						const index = i + j * imageData.width;
+						noiseNext[index] = noisePrev[index];
+					}
+
+					if (gridX == 1 && gridY == 1 && Math.random() < 0.8) {
+						const index = i + j * imageData.width;
+						noiseNext[index] = noisePrev[index];
+					}
+				}
+			}
+		}}
+		onrender={() => renderNoisePalette(noisePrev, imageData)}
 	></GraphicalEffect>
 </main>
 
