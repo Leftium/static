ac6283f78ad544c623a7ed9fc086f63f24eed222
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-05-11 22:55:55 +0900

    perf(static): precompute random values in bulk.

--- src/routes/static/+page.svelte
+++ src/routes/static/+page.svelte
@@ -6,44 +6,55 @@
 	let imageData: ImageData;
 	let noisePrev = new Uint8Array(0);
 	let noiseNext = new Uint8Array(0);
+
+	let noiseMask = new Uint8Array(0);
+	let randValues = new Uint8Array(0);
 </script>
 
 <main>
 	<GraphicalEffect
 		oninit={(fx: FxState) => {
 			//console.log('init', $state.snapshot(fx));
-			fx.scalingFactor = 1 / 4;
+			fx.scalingFactor = 1 / 1;
 			// Pixel ratio based on NTSC 440x486 resolution stretched to 4:3 aspect ratio.
 			fx.pixelAspectRatio = ((4 / 440) * 486) / 3;
 		}}
-		onresize={(_fx, width, height) => {
+		onresize={(fx, width, height) => {
 			console.log('resizeHandler', { width, height });
 			imageData = createOpaqueImageData(width, height);
 			noisePrev = new Uint8Array(width * height);
 			noiseNext = new Uint8Array(width * height);
+			noiseMask = new Uint8Array(width * height);
+			randValues = new Uint8Array(width * height);
 
 			generateNoiseUint8(noisePrev);
-			generateNoiseUint8(noiseNext);
-		}}
-		onupdate={(fx) => {
-			[noisePrev, noiseNext] = [noiseNext, noisePrev];
-
 			generateNoiseUint8(noiseNext);
 
 			for (let j = 0; j < imageData.height; j++) {
 				for (let i = 0; i < imageData.width; i++) {
 					const gridX = Math.floor(i / (100 / fx.pixelAspectRatio)) % 2;
 					const gridY = Math.floor(j / 100) % 2;
+					const index = i + j * imageData.width;
 
-					if (gridX == 0 && gridY == 0 && Math.random() < 1) {
-						const index = i + j * imageData.width;
-						noiseNext[index] = noisePrev[index];
+					if (gridX == 0 && gridY == 0) {
+						noiseMask[index] = 255;
 					}
 
-					if (gridX == 1 && gridY == 1 && Math.random() < 0.8) {
-						const index = i + j * imageData.width;
-						noiseNext[index] = noisePrev[index];
+					if (gridX == 1 && gridY == 1) {
+						noiseMask[index] = 255 * 0.8;
 					}
+				}
+			}
+		}}
+		onupdate={() => {
+			[noisePrev, noiseNext] = [noiseNext, noisePrev];
+
+			generateNoiseUint8(noiseNext);
+			generateNoiseUint8(randValues);
+
+			for (let i = 0; i < noiseNext.length; i++) {
+				if (randValues[i] < noiseMask[i]) {
+					noiseNext[i] = noisePrev[i];
 				}
 			}
 		}}
